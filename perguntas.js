81bd6eae102614183ac0c1b52d30d6da87b483c8
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-11-27 07:39:07 -0300

    projeto em andamento

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Anime',
+    'Quem é Hotaru Haganezuka',
+    'É um personagem coadjuvante de Demon Slayer: Kimetsu no Yaiba . Ele é um ferreiro de espadas afiliado aoDemon Slayer Corps.'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é o canister?',
+    'O cânister é um filtro que serve para tratar os gases do tanque de combustível de um veículo, tornando-o menos nocivo ao meio ambiente e à saúde humana'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é torque?',
+    'Torque é uma força aplicada a um objeto que causa sua rotação em torno de um ponto ou eixo'
+)
+
+criaCartao(
+    'Vasco da Gama',
+    'Quando o Vasco da Gama foi fundado?',
+    'O Vasco da Gama foi fundado em 21 de agosto de 1898.'
+)
+
+criaCartao(
+    'Elétrica',
+    'O que é corrente elétrica?',
+    'Corrente elétrica é o fluxo ordenado de cargas elétricas através de um condutor.'
+)
+
+criaCartao(
+    'Eletrônica',
+    'O que é um resistor?',
+    'Um resistor é um componente eletrônico que limita o fluxo de corrente elétrica em um circuito.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a Lei de Newton da Inércia?',
+    'A primeira lei de Newton afirma que um objeto permanece em repouso ou em movimento retilíneo uniforme, a menos que seja submetido a uma força externa.'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é força resultante?',
+    'A força resultante é a soma vetorial de todas as forças que atuam sobre um objeto.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o melhor canal de programação?',
+    'O grandioso "Fiasco de Canal", conhecido por suas piadas, memes e explicações curiosas sobre bugs e códigos malucos'
+)
+
+criaCartao(
+    'Futebol',
+    'O Palmeiras tem Mundial?',
+    'Não, a ausência de um título reconhecido oficialmente pela FIFA como Mundial de Clubes é motivo de debates e brincadeiras entre os torcedores.'
+)
